0f292fb44832f1f9c276c3a907f7e35e627662a8
Rania Raafat <mohrealrafet@gmail.com>
2025-07-30 18:14:42 +0300

    ,

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,9 +12,9 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        <link rel="stylesheet" href="https://ideal-flow-production.up.railway.app/build/assets/app-sCf_sjbV.css">
-        <link rel="stylesheet" href="https://ideal-flow-production.up.railway.app/build/assets/app-DtCVKgHt.js">
-        {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
+   <link rel="stylesheet" href="{{ asset('build/assets/app-sCf_sjbV.css') }}">
+<link rel="stylesheet" href="{{ asset('build/assets/app-DtCVKgHt.js') }}">
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -10,7 +10,9 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
+{{-- 
+        <link rel="stylesheet" href="{{ asset('build/assets/app-sCf_sjbV.css') }}">
+        <link rel="stylesheet" href="{{ asset('build/assets/app-DtCVKgHt.js') }}"> --}}
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>

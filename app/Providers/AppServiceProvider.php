c7822e092d8d41671ad7f28d2eae293bcd0b5a83
Rania Raafat <mohrealrafet@gmail.com>
2025-07-30 18:37:43 +0300

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,9 +20,10 @@ class AppServiceProvider extends ServiceProvider
    
     public function boot()
     {
-        if (env('APP_ENV') === 'production') {
-            URL::forceScheme('https');
-          }   
+        if (app()->environment('production')) {
+        URL::forceScheme('https');
+    }
+
 
     }
 

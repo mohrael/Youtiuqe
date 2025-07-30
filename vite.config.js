8eb74fb609c29167f1b9450ff20f1e4ef665284c
Rania Raafat <mohrealrafet@gmail.com>
2025-07-30 17:59:42 +0300

    .

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,14 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    base: '/build/', 
     plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
+        laravel([
+            'resources/css/app.css',
+            'resources/js/app.js',
+        ]),
     ],
+
     build: {
         outDir: 'public/build', 
         manifest: true,         
